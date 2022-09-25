9f6cc13a7d7aa5f56bdb25b2cba30b16d5fb7985
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-09-26 02:44:37 +0300

    use State to manage data

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import AccountContainer from "./AccountContainer";
 
 function App() {
+  const [transactions, setTransactions] = useState([])
   useEffect(() => (
     fetch ('http://localhost:8001/transactions')
       .then ((resp) => resp.json())
-      .then (function(transc){
-        console.log(transc)
+      .then (function(transaction){
+        setTransactions(transaction)
       })
-  ));
+  ),[]);
   return (
     <div className="ui raised segment">
       <div className="ui segment violet inverted">
